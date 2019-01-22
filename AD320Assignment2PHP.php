c59ad82bf473aba45656e7040bd4df3207fc55cd
Jon <jon.king047@gmail.com>
2019-01-21 19:47:53 -0800

    added assignment files

---
+++ AD320Assignment2PHP.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Name: <?php echo $_GET["Name"]; ?><br>
+Email: <?php echo $_GET["Email"]; ?><br>
+Phone Number: <?php echo $_GET["PhoneNumber"]; ?><br>
+State of Residence: <?php echo $_GET["State"]; ?><br>
+
+</body>
+</html>
